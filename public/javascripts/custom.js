1cab9906895fcfce9c221f5df4f6897c60d9309d
Tom Kuipers <t.f.kuipers@uva.nl>
2012-12-01 08:43:33 +0100

    sorting, voting, localstorage added

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -20,51 +20,45 @@ $(document).bind("mobileinit", function(){
   $.mobile.defaultPageTransition = "none";
 });
 
-
 var presentationId = presentationId || "";
 
 $("#pageListPresentations").live("pageinit", function() {
-	$.get("/api/presentations", function(data){ // of met trailing slash!
+	$.get("/api/presentations", function(data){ 
 		data.reverse();
                 var tempList = "";
 		$.each(data, function(index, value) {
 			tempList = tempList.concat('<li><a href="/' + value._id + '/" data-ajax="false">'+ value.title + '</a></li>');
-			//$("#presentationsListView").prepend('<li><a href="/' + value._id + '/" data-ajax="false">'+ value.title + '</a></li>');
 		});
 		$("#presentationsListView").append(tempList);
 		$("#presentationsListView").listview("refresh");
 	});
 });
 
-$("#pageListQuestions").live("pageinit", function() {
+$("#pageListQuestions").live("pagebeforeshow", function() {
 	$.get("/api/presentation/" + presentationId, function(data) {
 		var tempList = "";
 		$.each(data.questions, function(index, value) {
-			// tempList = tempList.concat('<li></li>');
-			tempList = tempList.concat( '<li>' + value.name + '<span class="ui-li-count" style="width: 1.5em; margin-right: 2em;"><a href="#" id="' + value._id + '" style="text-decoration: none;">+1</a></span><span class="ui-li-count">' + value.votes +'</span></li>');
-
+			var key = "Votes:" + presentationId  + ":" + value._id;
+			if (localStorage.getItem(key) === null) {
+				tempList = tempList.concat( '<li>' + value.name + '<span class="ui-li-count" style="width: 1.5em; margin-right: 2em;"><a href="#" id="' + value._id + '" style="text-decoration: none;">+1</a></span><span class="ui-li-count">' + value.votes +'</span></li>');
+                        } else {
+				tempList = tempList.concat( '<li>' + value.name + '<span class="ui-li-count">' + value.votes +'</span></li>');
+                        }
 		});
-		$("#questions").append(tempList);
+		$("#questions").html(tempList);
 		$("#questions").listview("refresh");
-		// add event listener for votingi
-		//$("#questions").on("click", "li a", function(event) {
-		$("#questions").find("li a").on("click", function(event) {
-			console.log("click event for voting: " + this.id);
+		// add event listener for voting
+		$("#questions").on("click", "li a", function(event) {
 			// store vote in localStorage
-
+			var key = "Votes:" + presentationId + ":" + this.id;
+			localStorage.setItem(key, "voted");
 			$.ajax({
 				url: "/api/presentation/" + presentationId,
 				type: "PUT",
 				data: {
 					"questionId": this.id,
 				},
-				success: function(data, textStatus, jqXHR) { 
-					console.log("add vote response: "); 
-					console.dir(data); 
-					console.log(textStatus); 
-					console.dir(jqXHR);
-					// get request om alle questions op te halen, localStorage bijhouden van uitgebrachte stemmen
-				}
+				success: window.location.reload()
 			});
 			event.preventDefault();
 		});
@@ -72,21 +66,17 @@ $("#pageListQuestions").live("pageinit", function() {
 });
 
 $("#pageQuestion").live("pageinit", function() {
-	console.log("#pageQuestions pageinit");
-
 	// populate previously asked questions
 	$.get("/api/presentation/" + presentationId, function(data) {
-		console.log(data);
- 		console.log("questions: " + data.questions.length);
 		$.each(data.questions, function(index, value) {
 			console.log(index + " , " + value);
-			$('#messages').prepend('<li><a href="#" id="' + value._id +'">' + value.name + '</a></li>');
+			$('#messages').prepend('<li><a href="#pageListQuestions" id="' + value._id +'">' + value.name + '</a></li>');
 		});
 		$('#messages').listview("refresh");
 	});
 
 	now.receiveMessage = function(name, message) {
-		$('#messages').prepend('<li><a href="#">' + message + '</a></li>');
+		$('#messages').prepend('<li><a href="#pageListQuestions">' + message + '</a></li>');
 		$('#messages').listview("refresh");
  	}
   
@@ -107,21 +97,21 @@ $("#pageQuestion").live("pageinit", function() {
 
 	now.ready(function() {
 		// set room to current presentationId
-		//now.changeRoom(presentationId);
-		now.changeRoom("50b3bc6de934806be6000002");
+		now.changeRoom(presentationId);
+		//now.changeRoom("50b3bc6de934806be6000002");
         });
 
 });
 
-$("#pageListFavorites").live("pageinit", function(){
+$("#pageListFavorites").live("pagebeforeshow", function(){ //pageinit + append(tempList)
 	console.log("#favoritesPage pageinit");
   // manipulate this page before its widgets are auto-initialized
 	$.get("/api/presentation/" + presentationId + "/favorites", function(data) {
-		console.log(data);
+		var tempList = "";
 		$.each(data, function(index, value) {
-			console.log(index + " , " + value);
-			$("#favorites").prepend( '<li>' + value.name + '<span class="ui-li-count" style="width: 1.5em; margin-right: 2em;"><a href="#" id="' + value._id + '" style="text-decoration: none;">+1</a></span><span class="ui-li-count">' + value.votes +'</span></li>');
+			tempList = tempList.concat('<li>' + value.name + '<span class="ui-li-count">' + value.votes +'</span></li>');
 		});
+		$("#favorites").html(tempList);
 		$("#favorites").listview("refresh");
 	}); 
 });
